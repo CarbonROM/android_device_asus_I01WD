4bbed388d2e96121c24c87b0187aad306aaff26e
Joe Maples <joe@maples.dev>
2020-02-14 20:59:22 -0500

    I01WD: Add Carbon configs
    Signed-off-by: Joe Maples <joe@maples.dev>

--- carbon_I01WD.mk
+++ carbon_I01WD.mk
@@ -5,14 +5,15 @@
 #
 
 $(call inherit-product, device/asus/I01WD/device.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common.mk)
+$(call inherit-product, vendor/carbon/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := asus
 PRODUCT_DEVICE := I01WD
 PRODUCT_MANUFACTURER := asus
 PRODUCT_MODEL := ASUS_I01WD
-PRODUCT_NAME := lineage_I01WD
+PRODUCT_NAME := carbon_I01WD
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
@@ -22,3 +23,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=WW_I01WD
 
 BUILD_FINGERPRINT := "asus/WW_I01WD/ASUS_I01WD:10/QP1A.190711.005/17.1810.1911.110-0:user/release-keys"
+
